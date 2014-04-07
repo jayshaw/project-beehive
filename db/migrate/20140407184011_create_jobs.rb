4afe05a27d4a4d063bd2963cfad78983b6ac88a4
Jay <jayshaw911@hotmail.com>
2014-04-07 13:52:20 -0500

    migrated jobs

--- db/migrate/20140407184011_create_jobs.rb
+++ db/migrate/20140407184011_create_jobs.rb
@@ -1,7 +1,9 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-
+      t.text :job_desc
+      t.string :duration
+      t.integer :pay
       t.timestamps
     end
   end
