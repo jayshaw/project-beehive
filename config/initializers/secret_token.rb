1bf686e4ba31ddc916ba1125e6cdecca750e076e
Jay <jayshaw911@hotmail.com>
2014-04-07 10:52:17 -0500

    New Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectBeehive::Application.config.secret_token = '518067fa88160ce712ad92f673e2e59ac92aebe6408bf4160c62b67f595a63c8f4892f1f3b312dd3b8384d05236317a97130342621757ea4f1e589274833a266'
